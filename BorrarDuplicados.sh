dc971e8f5b91eb343d76025d27e4e6851c6e2154
josea223 <usuarios@12servicios.com>
2017-05-01 12:03:59 -0400

    Update BorrarDuplicados.sh
    Modificado sistema de detección y eliminación de duplicados.
    Ahora se selecciona una única carpeta para analizar y dentro de ella se determina la que puede contener los duplicados a eliminar de manera prioritaria.

--- BorrarDuplicados.sh
+++ BorrarDuplicados.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-# Toca los archivos de la carpeta
-echo Carpeta con los duplicados a eliminar [ruta completa o relativa]
-read borrar
-# cd borrar
-echo Escribe no para evitar modificar la hora de cambio de los archivos de la carpeta [touch]
-read touch
-if [ "$touch" = 'no' ]; then echo Se borrarán los archivos según fecha de modificación actual.
-else
-find $borrar/* -print -exec touch {} \;
-echo Actualizada fecha de modificación de los archivos en $borrar
+# Introduce la ruta de la carpeta a analizar
+echo Carpeta a analizar en busca de duplicados a eliminar [ruta absoluta]
+read ruta
+
+# Actualizar la fecha de modificación de candidatos a duplicados [touch]
+# Evita que en caso de duplicados, aquellos en las restantes carpetas tengan priporidad para no ser eliminados.
+echo Carpeta con duplicados a eliminar modificando su fecha de modificación [ruta relativa/'']
+read duplicados
+if [ ! -z "$duplicados" ] && find $ruta/$duplicados/* -print -exec touch {} \;
 fi
-# Carpeta con los duplicados a conservar
-echo Carpeta con los duplicados a conservar
-read conservar
-echo Acción a realizar [borrar/prueba]
-read accion
-# Ejecuto fdupes
-if [ "$accion" = 'borrar' ]; then
-fdupes -rdN "$borrar" "$conservar" > ./logs/ArchivosEliminados.txt
-echo Archivos eliminados:
-cat ./logs/ArchivosEliminados.txt
-else fdupes -r "$borrar" "$conservar" > ./logs/ArchivosAEliminar.txt
+
+# Primer análisis de duplicados y muestra en pantalla
+fdupes -r "$ruta" > ./logs/ArchivosAEliminar.txt
 echo Archivos duplicados candidatos a ser eliminados:
 cat ./logs/ArchivosAEliminar.txt
+
+# Seleccionar la opción de borrar o no los duplicados
+echo Borrar los duplicados [si/no]
+read borrar_sn
+if [ "$borrar_sn" = 'si' ]; then
+fdupes -rdN "$ruta" > ./logs/ArchivosEliminados.txt
+echo Se eliminaron los siguientes archivos
+cat ./logs/ArchivosEliminados.txt
+echo Duplicados eliminados.
+else
+echo Acción cancelada. No se eliminó ningún archivo.
 fi
